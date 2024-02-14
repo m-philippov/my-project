a9d092f6624fe0cc06b1a7711897121649d2a6be
m-philippov <114732838+m-philippov@users.noreply.github.com>
2024-02-14 22:57:26 +0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "math/mul.h"
 #include "math/sum.h"
 
 int main() {
-    double c = sum(5.2, 3.4);
+    double c = sum(mul(5.2, 3.4),3.2);
     std::cout << c << std::endl;
     return 0;
 }
