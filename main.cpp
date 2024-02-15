f7feea66b3005e3295ae7ef82c403d0289ffec50
m-philippov <114732838+m-philippov@users.noreply.github.com>
2024-02-15 09:47:18 +0700

    New master minus

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "math/mul.h"
 #include "math/sum.h"
+#include "math/minus.h"
 
 int main() {
-    double c = sum(mul(5.2, 3.4),3.2);
+    double c = minus(sum(mul(5.2, 3.4),3.2), 3);
     std::cout << c << std::endl;
     return 0;
 }
