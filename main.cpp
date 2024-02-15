c76079015049d9206a38800b9e3f8d8807b1d1af
m-philippov <114732838+m-philippov@users.noreply.github.com>
2024-02-15 09:39:30 +0700

    New meta-code-modified

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "math/mul.h"
 #include "math/sum.h"
+#include "meta/meta.h"
 
 int main() {
     double c = sum(mul(5.2, 3.4),3.2);
     std::cout << c << std::endl;
+    // Пример обращения:
+    const unsigned f5 = factorial<5>::value; // 120
+    // Пример обращения:
+    const unsigned i = C<5, 2>::value; // 10
     return 0;
 }
